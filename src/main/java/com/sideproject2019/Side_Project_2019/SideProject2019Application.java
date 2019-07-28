faaf39c53b26920ab1c76d9ae6ae8ea8362f21ac
Camille Brunet <camille.brunet.07@hotmail.fr>
2019-07-28 19:48:30 +0200

    Configuration

---
+++ src/main/java/com/sideproject2019/Side_Project_2019/SideProject2019Application.java
@@ -0,0 +1,13 @@
+package com.sideproject2019.Side_Project_2019;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SideProject2019Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SideProject2019Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sideproject2019/Side_Project_2019/SideProject2019ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sideproject2019.Side_Project_2019;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SideProject2019ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
