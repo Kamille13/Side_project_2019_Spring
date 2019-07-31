1778f17c3e11817b6c97bc46489165347d33d211
Camille Brunet <camille.brunet.07@hotmail.fr>
2019-07-31 17:54:51 +0200

    Ajout model, controller et repository

---
+++ src/main/java/com/sideproject2019/Side_Project_2019/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.sideproject2019.Side_Project_2019.controller;
+
+import com.sideproject2019.Side_Project_2019.model.User;
+import com.sideproject2019.Side_Project_2019.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @PostMapping("/user/create")
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @PostMapping("/user/login")
+    public User login(@RequestBody User user) {
+        return userRepository.findByEmailAndPassword(user.getEmail(), user.getPassword());
+    }
+
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
---
+++ src/main/java/com/sideproject2019/Side_Project_2019/model/User.java
@@ -0,0 +1,85 @@
+package com.sideproject2019.Side_Project_2019.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+@Entity
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @NotEmpty
+    private String firstName;
+
+    @NotNull
+    @NotEmpty
+    private String lastName;
+
+    @NotNull
+    @NotEmpty
+    private String email;
+
+    @NotNull
+    @NotEmpty
+    @Size(min = 8)
+    private String password;
+
+    public User() {
+    }
+
+    public User(@NotNull @NotEmpty String firstName, @NotNull @NotEmpty String lastName, @NotNull @NotEmpty String email, @NotNull @NotEmpty String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
